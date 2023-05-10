ed3199c1e359ec362b2a5866181e479ec62cdb98
Aaradhya Sharma <aaradhyasharma@Aaradhyas-MacBook-Pro.local>
2023-05-10 14:19:41 +0530

    Initial Puch Backend

---
+++ backend.py
@@ -0,0 +1,32 @@
+import openai
+from distutils.log import debug
+from fileinput import filename
+from flask import *
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app) 
+
+def fetch_answer(question,docs):
+# Text completion API
+    openai.api_key = "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"
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=f"{docs}\n\nQ: {question}\nA:",
+    max_tokens=100,
+    temperature=0
+    )
+    print(question)
+    return response.choices[0].text.strip()
+    
+
+@app.route('/fileupload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    question = request.form['question']
+    answer = fetch_answer(question,file)
+    return {'answer' : answer}
+
+if __name__ == '__main__':
+    app.run(debug=True)
